bfb3c57f5c47eee21933589e1c6db45c871c1075
Nathanlovaniaina <142962456+Nathanlovaniaina@users.noreply.github.com>
2025-06-24 14:42:47 +0300

    commit

--- app/views/requeteClients.php
+++ app/views/requeteClients.php
@@ -23,7 +23,7 @@ $userRole = $_SESSION["role"] ?? 'client';
 
 <div class="main-content">
     <h1>Gestion des Requêtes Clients</h1>
-    
+    <a href="formulaireRequete" class="add-btn">➕ Ajouter une action</a>
     <!-- Formulaire de filtre -->
     <div class="filter-container">
         <form method="get" action="<?= $base_url ?>/requeteClient" class="filter-form">
